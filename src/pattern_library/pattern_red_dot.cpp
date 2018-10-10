723a3cffc2ecfbec39e92f4f43acd66de5ab06f9
Brian Cruz <cruz.s.brian@gmail.com>
2018-10-09 20:20:23 -0700

    Make red dot non-blocking

--- src/pattern_library/pattern_red_dot.cpp
+++ src/pattern_library/pattern_red_dot.cpp
@@ -7,17 +7,26 @@
  * POT -> change dot speed.
  */
 
+int i;
+
 void Pattern_Red_Dot::init() {
     clearLEDs();
     FastLED.setBrightness(BRIGHTNESS_MAX);
+
+    i = 0;
 }
 
 void Pattern_Red_Dot::loop() {
-    for (int i = 0; i < NUM_LEDS_TOTAL; i++) {
-      leds[transform(i)] = CRGB::Red;
-      leds[transform(i + 1)] = CRGB::Red;
-      leds[transform(i - 1)] = CRGB::Black;
-      FastLED.show();
-      delay(map(analogRead(PIN_POT), 0, POT_MAX, 1, 120));
+    //for (int i = 0; i < NUM_LEDS_TOTAL; i++) {
+    leds[transform(i)] = CRGB::Red;
+    leds[transform(i + 1)] = CRGB::Red;
+    leds[transform(i - 1)] = CRGB::Black;
+    FastLED.show();
+    delay(map(analogRead(PIN_POT), 0, POT_MAX, 1, 120));
+
+    i++;
+    if (i >= NUM_LEDS_TOTAL) {
+        i = 0;
     }
+    //}
 }
