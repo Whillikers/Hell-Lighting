882e007f506bf11893229d8f653a0e1b97bdb963
Brian Cruz <cruz.s.brian@gmail.com>
2018-10-21 16:32:05 -0700

    Speed up marbling by splitting leds into groups

--- src/pattern_library/pattern_marbling.cpp
+++ src/pattern_library/pattern_marbling.cpp
@@ -8,9 +8,15 @@ void Pattern_Marbling::init() {
 void Pattern_Marbling::loop() {
     hue = map(analogRead(PIN_POT), 0, POT_MAX, 0, 255);
 
-    for (int i = 0; i < NUM_LEDS_TOTAL; i++) {
-        int brightness = random(0, brightness_levels) * (255 / brightness_levels);
-        leds[transform(i)] = CHSV(hue, 255, brightness);
+    for (int i = 0; i < NUM_LEDS_TOTAL / led_groups; i++) {
+        int b = random(0, brightness_levels);
+        int brightness = b * brightness_step + min_brightness;
+        leds[i * led_groups + group] = CHSV(hue, 255, brightness);
+    }
+
+    group++;
+    if (group >= led_groups) {
+        group = 0;
     }
 
     FastLED.show();
--- src/pattern_library/pattern_marbling.h
+++ src/pattern_library/pattern_marbling.h
@@ -7,6 +7,10 @@ public:
     void loop();
     void init();
 private:
+    const int brightness_levels = 5;
+    const int min_brightness = 40;
+    const int brightness_step = (255 - min_brightness) / brightness_levels;
+    int led_groups = 4;
+    int group = 0;
     int hue;
-    const int brightness_levels = 10;
 };
